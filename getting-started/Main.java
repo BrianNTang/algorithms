af06431963dfe25952c275fea7140af7b6566031
BrianNTang <38578221+BrianNTang@users.noreply.github.com>
2025-05-12 22:45:14 -0500

    Adds insertion sort

--- getting-started/Main.java
+++ getting-started/Main.java
@@ -1,17 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
-	public static void main(String[] args) {
-		int[] unsorted = new int[]{9,8,7,6,5,4,3,2,1,0};
-    int[] sorted = new int[]{0,1,2,3,4,5,6,7,8,9};
+    public static void main(String[] args) {
+        int[] unsorted = new int[] { 2, 8, 1, 3, 6, 9, 7, 5, 0, 4 };
+        int[] sorted = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    int[] result = insertion_sort(unsorted);
-    if (!sorted.equals(result)) {
-      throw new IllegalStateException();
+        int[] result = insertion_sort(unsorted);
+        if (!Arrays.equals(sorted, result)) {
+            System.out.println(Arrays.toString(result));
+            throw new IllegalStateException("Not sorted");
+        }
+        System.out.println("Well done.");
     }
-    System.out.println("Well done.");
-	}
 
-  public static int[] insertion_sort(int[] unsorted) {
-    int[] result = new int[unsorted.length];
-    return result;
-  }
+    public static int[] insertion_sort(int[] unsorted) {
+        int[] result = Arrays.copyOf(unsorted, unsorted.length);
+
+        for (int i = 1; i < result.length; i++) {
+            int key = result[i];
+            int j = i - 1;
+            while (j >= 0 && result[j] > key) {
+                result[j + 1] = result[j--];
+            }
+            result[j + 1] = key;
+        }
+
+        return result;
+    }
 }
